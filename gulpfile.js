704b4e6e1b896239ca028dfe4bee0a5b0cb3d347
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2018-03-23 22:08:43 +0800

    Update gulpfile JS script

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp 					= require('gulp'),
+		requireDir		= require('require-dir'),
+		tasks					= requireDir('./gulp/tasks', { recurse: true });
+
+// Include paths file
+var paths 				= require('./gulp/paths');
+
+// 'gulp build' -- build assets
+gulp.task('build', gulp.series(
+	gulp.series('styles', 'scripts', 'html', 'images')
+));
+
+// 'gulp' -- default task execution
+gulp.task('default', gulp.series('build', 'serve'));
