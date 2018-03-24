6e67a612b94d70ff2246e75fe3d91da061b36ee8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2018-03-24 09:42:54 +0800

    Optimize scripts and styles Gulp tasks

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,10 +1,9 @@
 'use strict';
 
-var autoprefixer = require('autoprefixer'),
+var autoprefixer = require('gulp-autoprefixer'),
 		browsersync  = require('browser-sync').create(),
 		concat       = require('gulp-concat'),
 		concatcss    = require('gulp-concat-css'),
-		cssimport    = require('gulp-cssimport'),
 		csslint      = require('gulp-csslint'),
 		cssnano      = require('gulp-cssnano'),
 		gulp         = require('gulp'),
@@ -13,7 +12,7 @@ var autoprefixer = require('autoprefixer'),
 		imagemin 		 = require('gulp-imagemin'),
 		notify       = require('gulp-notify'),
 		plumber      = require('gulp-plumber'),
-		postcss      = require('gulp-postcss'),
+		rename			 = require('gulp-rename'),
 		sass         = require('gulp-sass'),
 		size         = require('gulp-size'),
 		sourcemaps   = require('gulp-sourcemaps'),
@@ -23,39 +22,36 @@ var autoprefixer = require('autoprefixer'),
 var paths 			 = require('../paths');
 
 // 'gulp styles' -- compiles SCSS files into a single CSS file, adds prefixes, creates a sourcemap, and auto-inject into browsers
-var processors = [
-	autoprefixer({
-		browsers: [
-			'last 2 versions',
-			'> 1%',
-			'ie >= 9',
-			'ie_mob >= 10',
-			'ff >= 30',
-			'chrome >= 34',
-			'safari >= 7',
-			'opera >= 23',
-			'ios >= 7',
-			'android >= 4',
-			'bb >= 10'
-		]
-	}),
-	cssnano,
-	csslint
-];
-
 gulp.task('styles', () => 
-	gulp.src(paths.scssSourceFiles + 'style.scss')
+	gulp.src([
+		paths.vendorSourceFiles + 'bootstrap/scss/bootstrap.scss',
+		paths.scssSourceFiles + 'style.scss',
+	])
 		.pipe(plumber())
-		.pipe(sass({
-			outputStyle: 'compressed',
-			includePaths: [
-				paths.scssFilesGlob
-			]
-		}).on('error', sass.logError))
-		.pipe(cssimport())
+		.pipe(sass({ style: 'expanded' }).on('error', sass.logError))
 		.pipe(concatcss('style.css'))
-		.pipe(postcss(processors))
-		.pipe(plumber.stop())		
+		.pipe(autoprefixer({
+			 browsers: [
+				'last 2 versions',
+				'> 1%',
+				'ie >= 9',
+				'ie_mob >= 10',
+				'ff >= 30',
+				'chrome >= 34',
+				'safari >= 7',
+				'opera >= 23',
+				'ios >= 7',
+				'android >= 4',
+				'bb >= 10'
+			]
+		}))
+		.pipe(gulp.dest(paths.cssPublicFiles))
+		.pipe(cssnano())	
+		.pipe(rename({
+			basename: 'style',
+			suffix: '.min'
+		}))
+		.pipe(plumber.stop())	
 		.pipe(sourcemaps.init())
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(paths.cssPublicFiles))
@@ -72,11 +68,14 @@ gulp.task('styles', () =>
 // 'gulp scripts' -- compiles JS files into a single JS file, creates a sourcemap, and auto-inject into browsers
 gulp.task('scripts', () =>
 	gulp.src([
-		paths.vendorSourceFiles + 'jquery/dist/jquery.js',
+		paths.vendorSourceFiles + 'jquery/dist/jquery.slim.js',
+		paths.vendorSourceFiles + 'popper.js/dist/umd/popper.js',
+		paths.vendorSourceFiles + 'bootstrap/dist/js/bootstrap.js',
 		paths.jsFilesGlob
 	])
 		.pipe(plumber())
 		.pipe(concat('bundle.js'))
+		.pipe(gulp.dest(paths.jsPublicFiles))
 		.pipe(uglify({
 			mangle: true,
 			compress: {
@@ -89,6 +88,10 @@ gulp.task('scripts', () =>
 				join_vars: true,
 				drop_console: true
 			}
+		}))
+		.pipe(rename({
+			basename: 'bundle',
+			suffix: '.min'
 		}))
 		.pipe(sourcemaps.init())
 		.pipe(sourcemaps.write('.'))
@@ -153,8 +156,8 @@ gulp.task('serve', (done) => {
 		injectChanges: true
 	});
 
-	gulp.watch(paths.scssFilesGlob, gulp.series('styles', reload));
-	gulp.watch(paths.jsFilesGlob, gulp.series('scripts', reload));
-	gulp.watch(paths.imageFilesGlob, gulp.series('images', reload));
-	gulp.watch(paths.htmlFilesGlob, gulp.series('html', reload));
+	gulp.watch(paths.scssFilesGlob, gulp.series('clean::css', 'styles', reload));
+	gulp.watch(paths.jsFilesGlob, gulp.series('clean::js', 'scripts', reload));
+	gulp.watch(paths.imageFilesGlob, gulp.series('clean::images', 'images', reload));
+	gulp.watch(paths.htmlFilesGlob, gulp.series('clean::html', 'html', reload));
 });
